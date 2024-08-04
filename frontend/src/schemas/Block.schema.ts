869a114ebe1e1d4f51dcd228ab784752ac6eb99c
Robin <rob.blanquart02100@gmail.com>
2024-08-04 22:51:26 +0200

    fix: fetch node is working

--- frontend/src/schemas/Block.schema.ts
+++ frontend/src/schemas/Block.schema.ts
@@ -2,11 +2,11 @@ import { z } from 'zod';
 import { addressSchema } from './Address.schema';
 
 export const blockSchema = z.object({
-  id: z.string().min(1),
+  id: z.string().optional(),
   height: z.number(),
   round: z.number().nullable().default(0),
-  block_id: z.string().min(1),
-  address: addressSchema.shape.id,
+  block_id: z.string().optional(),
+  address: addressSchema.shape.id.optional(),
   c001: z.number(),
   c002: z.number(),
   c003: z.number(),
