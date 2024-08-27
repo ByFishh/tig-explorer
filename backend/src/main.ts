dbbf5a174590f1486489bb39f7137c534c5c0088
ByFish <maringertheo@gmail.com>
2024-08-27 22:23:08 +0200

    feat: remove cors in production

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,9 +4,13 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: '*',
-  });
+
+  if (process.env.NODE_ENV !== 'production') {
+    app.enableCors({
+      origin: '*',
+    });
+  }
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
